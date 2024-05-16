fcda56e58af5d1bacbbd899008dc94c101a80ad6
RYU HYEON SEOK <gustjr1058@naver.com>
2024-05-16 19:34:40 +0900

    login

--- db_model/mysql.py
+++ db_model/mysql.py
@@ -4,8 +4,8 @@ MYSQL_HOST = 'localhost'
 MYSQL_CONN = pymysql.connect(
     host=MYSQL_HOST,
     port=3306,
-    user='root',
-    passwd='@ks24764056',
+    user='yourdbusername',
+    passwd='yourdbpassword',
     db='fairy_tail',
     charset='utf8')
 
